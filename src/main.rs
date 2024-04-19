07b3006ea38d89e9ed41ff5cce892ec8cfa3565c
Chad DeRosier <chad.derosier@tutanota.com>
2024-04-18 18:01:37 -0700

    thing

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use van_colleague::session::Session;
 #[tokio::main]
 async fn main() {
     let mut session = Session {
+        ble_name: "VanColleague".to_string(),
         ..Default::default()
     };
 
